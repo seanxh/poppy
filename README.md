74101c9575f8f4614fbdb9f34a72a4eb822b7d61
seanxh <seanxuhao@gmail.com>
2014-03-01 21:48:00 +0800

    update readme.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ poppy
 =====
 
 poppy is a dir's synchronize tool
+
+Start
+
+python main.py
+
+Config
+
+Edit config.py
+[r"DIR1",r"DIR2",[patterns]]
--- config.py
+++ config.py
@@ -4,14 +4,7 @@
 ##要同步的文件夹~
 ##每个ITEM包含要同步的两个文件夹的名字
 DIRS = (
-    [r"D:/openplatform/protected/",r"Z:/open.rms.baidu.com/protected/"],
-    [r"D:/rms_249-4/protected/",r"Z:/rms/atm-249-4_BRANCH/protected/",['.*\.html']],
-    [r"D:/rms_249-4/protected/modules/",r"Z:/rms/atm-249-4_BRANCH/protected/modules/",['.*\.html']],
-    [r"D:/rms_249-4/protected/modules/fast_rename/",r"Z:/rms/atm-249-4_BRANCH/protected/modules/fast_rename/"],
-    [r"D:/rms_249-4/protected/modules/rms_queue/",r"Z:/rms/atm-249-4_BRANCH/protected/modules/rms_queue/"],
-    [r"D:/rms_249-4/protected/modules/person/controllers/",r"Z:/rms/atm-249-4_BRANCH/protected/modules/person/controllers/"],
-    [r"D:/rms_249-4/protected/controllers/",r"Z:/rms/atm-249-4_BRANCH/protected/controllers/"],
-    [r"D:/rms_249-4/protected/handle_data_queue/",r"Z:/rms/atm-249-4_BRANCH/protected/handle_data_queue/"],
-    [r"D:/rms_240/protected/modules/app/",r"Z:/rms/atm-240_BRANCH/protected/modules/app/"],
-    [r"D:/Monitor2/protected/",r"Z:/monitor/protected/"]
+    [r"D:/openplatform/protected/",r"Z:/yii/protected/"],
+    [r"D:/yii/protected/",r"Z:/yii/protected/",['.*\.html']],
+    [r"D:/yii/protected/modules/",r"Z:/yii/protected/modules/",['.*\.html']],
 )
